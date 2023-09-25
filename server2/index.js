041c904d8805b1a877842a19115a7042d6f3dc3d
LautaDurante <durantelautarop@gmail.com>
2023-09-25 16:21:07 -0300

    trabajo finalizado

---
+++ server2/index.js
@@ -0,0 +1,20 @@
+import mysql from 'mysql2';
+import express from 'express';
+
+const port = 80;
+const app = express();
+
+const pool = mysql.createPool({
+    host:'172.17.0.3',
+    user:'root',
+    password:'root1',
+    database:'prueba',
+}).promise();
+
+
+app.get('/', async (req, res) => {
+    const result = await pool.query('SELECT * FROM ALUMNOS');
+    res.json({'web':2, 'result':result[0]});
+});
+
+app.listen(port, ()=>{console.log(`listening on port ${port}`);});
